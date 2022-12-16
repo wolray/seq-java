ec12a3db08fff92f1390a373307c28161d11e064
wolray <290061869@qq.com>
2022-12-16 16:53:52 +0800

    [fix] PickItr stop

--- src/main/java/com/github/wolray/seq/PickItr.java
+++ src/main/java/com/github/wolray/seq/PickItr.java
@@ -20,7 +20,7 @@ public abstract class PickItr<T> implements Seq<T>, Iterator<T> {
             public T pick() {
                 while (!cur.hasNext()) {
                     if (!iterator.hasNext()) {
-                        Foldable.stop();
+                        stop();
                     }
                     cur = iterator.next().iterator();
                 }
